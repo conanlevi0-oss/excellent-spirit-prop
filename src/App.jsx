5e7bd6ea59f9374f548a418a6f585c0f2a9a0b9b
MACBOOK PRO 2015 <macbookpro2015@Conan.local>
2026-03-12 18:20:40 +0300

    Fix: scroll to top on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Services from './pages/Services';
@@ -13,6 +14,7 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <ScrollToTop />
         <Navbar />
         <main>
           <Routes>
